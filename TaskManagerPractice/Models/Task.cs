8ca83f5b4f4a01f7b14c1d8dbf377cb38c321698
EitanVeryKatz <eitanka@mta.ac.il>
2025-04-12 20:30:31 +0300

    Added new Task Module

---
+++ TaskManagerPractice/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerPractice.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+
+        [Required]
+        public DateOnly DeadlineDate { get; set; }
+
+        public bool WasCompleted { get; set; }
+    }
+}
